c22c589b8b5c41edccc4e6a57cfcdbf6cbd2f86c
Linet <muthiigathoni@gmail.com>
2023-06-19 01:19:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun(activity);
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+mondayWork(work);
+
+function wrapAdjective(asterik = "*"){
+    return function emphatic(message = "a hard worker"){
+        return `You are ${asterik}${message}${asterik}!`;
+    }
+}
+wrapAdjective("||")("dedicated programmer")
